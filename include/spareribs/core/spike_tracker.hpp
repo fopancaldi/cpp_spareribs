743d0e72cb54086d9e860fa78d2b346e954b7509
Francesco Orso Pancaldi <105540514+fopancaldi@users.noreply.github.com>
2026-08-21 14:48:34 +0200

    Fix: do not count threshold crossing (due to noise) when recharging as spike

--- include/spareribs/core/spike_tracker.hpp
+++ include/spareribs/core/spike_tracker.hpp
@@ -22,12 +22,12 @@ __device__ T& ptr_at(T* ptr, std::size_t y_idx, std::size_t x_idx, std::size_t x
 
 template <std::floating_point F>
 __global__ void update_kernel(F* spike_times, std::size_t* written_elems, F const* u_future,
-                              F const* u_past, F current_time, F threshold,
+                              F const* u_past, F const* v_future, F current_time, F threshold,
                               std::size_t max_written_elems, std::size_t simulations) {
     assert(threadIdx.y == 0 and threadIdx.z == 0);
     unsigned int const g_idx = blockIdx.x * blockDim.x + threadIdx.x;
     if (g_idx < simulations) {
-        if (u_future[g_idx] > threshold and u_past[g_idx] < threshold) {
+        if (u_future[g_idx] > threshold and u_past[g_idx] < threshold and v_future[g_idx] < F{0}) {
             std::size_t const spikes_current_sim = written_elems[g_idx];
             assert(spikes_current_sim + 1 <= max_written_elems);
             ptr_at(spike_times, g_idx, spikes_current_sim, max_written_elems) = current_time;
@@ -129,8 +129,8 @@ class SpikeTracker {
         assert(past.len() == simulations_);
         unsigned int const grid_blocks = div_ceil(simulations_, block_threads_);
         detail::spike_tracker::update_kernel<<<grid_blocks, block_threads_>>>(
-            spike_times_, written_elems_, future.u(), past.u(), future.time(), threshold_,
-            max_written_elems_, simulations_);
+            spike_times_, written_elems_, future.u(), past.u(), future.v(), future.time(),
+            threshold_, max_written_elems_, simulations_);
     }
     std::size_t total_spikes() const {
         std::size_t result;
